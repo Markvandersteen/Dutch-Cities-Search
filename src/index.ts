63e7a8a05fdba9d40ef724213f642f778651cacc
Mark van der Steenhoven <mark.van.der.steenhoven@postcodeloterij.nl>
2023-10-30 11:13:36 +0100

    setting up build scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-export * from './search';
+import { CitiesFetcher, Fetcher } from './getCities';
+import * as fs from 'fs';
+import { City } from './types/city';
+import { searchCity } from './search';
+
+const FILE_PATH = 'dist/cities.json';
+
+const writeCitiesToDist = async (cities: City[]) => {
+    return fs.writeFileSync(FILE_PATH, JSON.stringify(cities), 'utf8');
+};
+
+const readCities = (): City[] => {
+    const json = fs.readFileSync(FILE_PATH, 'utf8');
+    return JSON.parse(json) as City[];
+};
+export const updateCities = async (dataFetcher: Fetcher<City>) => {
+    const cities = await dataFetcher.fetchData();
+    writeCitiesToDist(cities);
+};
+
+export const search = async (needle: string, dataFetcher: Fetcher<City>) => {
+    if (fs.existsSync(FILE_PATH)) {
+        return searchCity(readCities(), needle)[0];
+    }
+    const haystack = await dataFetcher.fetchData();
+    writeCitiesToDist(haystack);
+    return searchCity(haystack, needle)[0];
+};
+
+console.log(search('BERKEL', new CitiesFetcher()));

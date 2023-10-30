572cc8ee537dd256cc61e8fc0ae0566b519a26e7
Mark van der Steenhoven <mark.van.der.steenhoven@postcodeloterij.nl>
2023-10-30 13:14:54 +0100

    removed unused variable

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,11 +3,6 @@ import { getCitiesDataToJson } from './getCities';
 
 describe('writeCitiesToDist', () => {
     it('should write cities to file and check if it exists', async () => {
-        const cities = [
-            { title: 'Amsterdam', key: 123, categoryGroupId: 123 },
-            { title: 'Rotterdam', key: 123, categoryGroupId: 123 },
-            { title: 'Utrecht', key: 123, categoryGroupId: 123 },
-        ];
         await getCitiesDataToJson();
         const fileExists = fs.existsSync('dist/cities.json');
         expect(fileExists).toBe(true);
